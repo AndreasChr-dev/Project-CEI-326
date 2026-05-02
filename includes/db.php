6f3877f1f0de517b3827ccdbe6ce4a3c5ef33b8b
AndreasChr-dev <andn.christodoulou@hotmail.com>
2026-05-02 13:13:07 +0300

    Update database connection config (AM 27937)

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@ try {
     $pdo = new PDO(
         'mysql:host=localhost;dbname=cei326_lab;charset=utf8mb4',
         'root',   
-        'user@mysql',       
+        '',       
         [
             
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
